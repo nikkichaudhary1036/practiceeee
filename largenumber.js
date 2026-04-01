04ac3818e980a65d7919a786994ffb54cc695833
Nikkyy <nikkichaudhary402@gmail.com>
2026-04-01 21:03:46 +0530

    Implement getElements function to log larger numbers
    Fix function name and variable references.

---
+++ largenumber.js
@@ -0,0 +1,10 @@
+let array = [3,4,5,6,7,8];
+let num = [2];
+function get elements(array, num) {
+     for(i=0; i < array.length; i++){
+        if (arr[i] > num){
+            console.log(arr[i]);g
+        } 
+     }
+}
+getElements(arr, num);
